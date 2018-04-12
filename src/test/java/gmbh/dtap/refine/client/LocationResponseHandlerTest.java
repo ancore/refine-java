42f47a966610631e668fb734f748f660a5bfd8b3
Andreas Cordsen <andreas@cordsen.de>
2018-04-12 23:48:34 +0200

    Clean-up of unit tests.

--- src/test/java/gmbh/dtap/refine/client/LocationResponseHandlerTest.java
+++ src/test/java/gmbh/dtap/refine/client/LocationResponseHandlerTest.java
@@ -2,21 +2,22 @@ package gmbh.dtap.refine.client;
 
 import gmbh.dtap.refine.api.RefineException;
 import gmbh.dtap.refine.api.RefineProjectLocation;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
+import org.apache.http.message.BasicHeader;
 import org.json.JSONException;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import static gmbh.dtap.refine.test.HttpMock.mockHttpResponse;
+import static org.apache.http.HttpHeaders.LOCATION;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 
 /**
@@ -27,57 +28,54 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class LocationResponseHandlerTest {
 
+   @Rule public ExpectedException thrown = ExpectedException.none();
    private LocationResponseHandler locationResponseHandler;
-   @Mock private HttpResponse httpResponse;
-   @Mock private StatusLine statusLine;
-   @Mock private Header header;
 
    @Before
    public void setUp() throws MalformedURLException {
       locationResponseHandler = new LocationResponseHandler();
    }
 
+
    @Test
-   public void should_return_location() throws IOException, JSONException {
+   public void should_throw_exception_when_response_status_is_200() throws IOException {
+      HttpResponse httpResponse = mockHttpResponse(200);
+
+      thrown.expect(RefineException.class);
+      locationResponseHandler.handleResponse(httpResponse);
+   }
+
+   @Test
+   public void should_throw_exception_when_response_status_is_500() throws IOException {
+      HttpResponse httpResponse = mockHttpResponse(500);
+
+      thrown.expect(RefineException.class);
+      locationResponseHandler.handleResponse(httpResponse);
+   }
+
+   @Test
+   public void should_throw_exception_when_location_header_is_missing() throws IOException {
+      HttpResponse httpResponse = mockHttpResponse(302);
+      when(httpResponse.getHeaders(LOCATION)).thenReturn(null);
+      when(httpResponse.getFirstHeader(LOCATION)).thenReturn(null);
+      when(httpResponse.getLastHeader(LOCATION)).thenReturn(null);
+      when(httpResponse.getAllHeaders()).thenReturn(null);
+
+      thrown.expect(RefineException.class);
+      locationResponseHandler.handleResponse(httpResponse);
+   }
+
+   @Test
+   public void should_return_location_when_location_header_is_available() throws IOException, JSONException {
       String projectId = "617613761";
       String locationHeaderValue = "http://localhost:8080/project?project=" + projectId;
       RefineProjectLocation expectedLocation = MinimalRefineProjectLocation.from(locationHeaderValue);
 
-      when(statusLine.getStatusCode()).thenReturn(302);
-      when(httpResponse.getStatusLine()).thenReturn(statusLine);
-      when(header.getValue()).thenReturn(locationHeaderValue);
-      when(httpResponse.getFirstHeader("Location")).thenReturn(header);
+      HttpResponse httpResponse = mockHttpResponse(302, new BasicHeader(LOCATION, locationHeaderValue));
 
       RefineProjectLocation actualLocation = locationResponseHandler.handleResponse(httpResponse);
       assertThat(actualLocation).isNotNull();
       assertThat(actualLocation.getId()).isEqualTo(expectedLocation.getId());
       assertThat(actualLocation.getUrl()).isEqualTo(expectedLocation.getUrl());
-   }
-
-   @Test
-   public void should_throw_exception_on_unexpected_status() throws IOException {
-      when(statusLine.getStatusCode()).thenReturn(200);
-      when(httpResponse.getStatusLine()).thenReturn(statusLine);
-
-      try {
-         locationResponseHandler.handleResponse(httpResponse);
-         fail("expected exception not thrown");
-      } catch (RefineException expectedException) {
-         assertThat(expectedException.getMessage()).isEqualTo("Unexpected response status: 200");
-      }
-   }
-
-   @Test
-   public void should_throw_exception_on_missing_header() throws IOException {
-      when(statusLine.getStatusCode()).thenReturn(302);
-      when(httpResponse.getStatusLine()).thenReturn(statusLine);
-      when(httpResponse.getFirstHeader("Location")).thenReturn(null);
-
-      try {
-         locationResponseHandler.handleResponse(httpResponse);
-         fail("expected exception not thrown");
-      } catch (RefineException expectedException) {
-         assertThat(expectedException.getMessage()).isEqualTo("No location header found.");
-      }
    }
 }
